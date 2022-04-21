d68ee0570b3e5f9f28d52810269a2b859ce151a0
Ulvi Shikhlinskiy <ushikhlinskiy@mobiquityinc.com>
2022-04-21 14:11:15 +0200

    added vnet

---
+++ vnet/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+    features {}
+    }
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+}
